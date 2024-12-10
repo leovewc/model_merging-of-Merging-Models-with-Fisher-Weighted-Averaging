96db78aa03c8708a4a0d01c187b9489186100f8c
leovewc <1285737503@qq.com>
2024-12-10 17:05:20 +0800

    add test and install

---
+++ install.py
@@ -0,0 +1,11 @@
+import logging
+from transformers import AutoModel, AutoTokenizer
+import os
+os.environ['HF_HOME'] = '/root/autodl-tmp/cache/' #change to your own location
+
+logging.basicConfig(level=logging.INFO)
+
+# download model
+model_name = "sgugger/glue-mrpc"
+model = AutoModel.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
